2bd7e358c5c65d6e51683e9a387e64b97ac5b09c
Stefan Dierauf <sdierauf@gmail.com>
2015-06-05 11:41:46 -0700

    no longer using json-server

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-npm install -g json-server
+# npm install -g json-server
 npm run deps
 gulp
 gulp serve &
-json-server db.json -p 4000
+# json-server db.json -p 4000
 
 echo "app server running at localhost:3000"
-echo "API server running at localhost:4000"
+echo "API server running at resplendent-inferno-164.firebaseio.com"
